a94d2dd723739bb47e15cad106abdf81be4c0195
Jeff Ma <jeffma@jeffs-mbp.myfiosgateway.com>
2020-07-22 17:17:20 -0400

    UI patches

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+source dcyphr-env/bin/activate
 export FLASK_APP=application.py FLASK_ENV=development DATABASE_URL=postgres://renmhhpugjueqo:ad87849e90586574f45a3b5eeb037d5c87d6d1edad3c9da35ed0d8d7e3b34ad2@ec2-3-230-106-126.compute-1.amazonaws.com:5432/d64lerqdbpjdga
 flask run
